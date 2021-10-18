c008f704585c6d176938619f6b72d5b9fb872b41
Xander Atalay <72700583+XanderAtalay@users.noreply.github.com>
2021-10-18 11:00:46 -0400

    Adding Boston and Chicago Sentiment Analysis

---
+++ Northeast+Midwest.R
@@ -0,0 +1,58 @@
+library(tidyverse)
+library(tidytext)
+library(ggwordcloud)
+library(gutenbergr)
+library(textdata)
+
+BostonMagazineClimate <- tibble(text = read_lines('BostonMagazineClimate.txt'))
+
+BM_Words <-  BostonMagazineClimate %>% unnest_tokens(word, text)
+BM_Words_SW <- BM_Words %>% anti_join(stop_words)
+BM_Count <- BM_Words_SW %>% count(word, sort=TRUE)
+
+BMSentiment_affin <- BM_Words %>%
+  inner_join(get_sentiments("afinn"))
+
+BMSentiment_nrc <- BM_Words %>%
+  inner_join(get_sentiments("nrc"))
+
+BMSentiment_bing <- BM_Words %>%
+  inner_join(get_sentiments("bing"))
+
+table(BMSentiment_bing$sentiment)
+table(BMSentiment_nrc$sentiment)
+
+(ggplot(data = BMSentiment_affin, 
+       aes(x=value))+
+  geom_histogram()+
+  ggtitle("Boston Magazine Climate Sentiment Range")+
+  theme_minimal())
+
+
+
+ChicagoHeraldClimate <- tibble(text = read_lines('ChicagoHeraldClimate.txt'))
+
+CH_Words <-  ChicagoHeraldClimate %>% unnest_tokens(word, text)
+CH_Words_SW <- CH_Words %>% anti_join(stop_words)
+CH_Count <- CH_Words_SW %>% count(word, sort=TRUE)
+
+CHSentiment_affin <- CH_Words %>%
+  inner_join(get_sentiments("afinn"))
+
+CHSentiment_nrc <- CH_Words %>%
+  inner_join(get_sentiments("nrc"))
+
+CHSentiment_bing <- CH_Words %>%
+  inner_join(get_sentiments("bing"))
+
+table(CHSentiment_bing$sentiment)
+table(CHSentiment_nrc$sentiment)
+
+(ggplot(data = CHSentiment_affin, 
+        aes(x=value))+
+    geom_histogram()+
+    ggtitle("Boston Magazine Climate Sentiment Range")+
+    theme_minimal())
+
+
+
